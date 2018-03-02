3a1ba6be7a94fb639d1a5472de04a62ecc631ab6
Lin Yang <lin.a.yang@intel.com>
2018-03-01 16:10:54 -0800

    Refactor telemetry definition
    Merged all kinds of telemetry definition variables into one single
    class instead of separate files.
    
    Change-Id: Id772b370019af958413390484e1a0166c34ebfef

--- rsd_lib/resources/v2_2/telemetry/metric_definitions/metric_definitions.py
+++ rsd_lib/resources/v2_2/telemetry/metric_definitions/metric_definitions.py
@@ -13,61 +13,80 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import os
-
 from sushy.resources import base
 
-from rsd_lib.resources.v2_2.telemetry.metric_definitions import cpu_bandwidth
-from rsd_lib.resources.v2_2.telemetry.metric_definitions import cpu_health
-from rsd_lib.resources.v2_2.telemetry.metric_definitions import cpu_temperature
+
+class MetricDefinition(base.ResourceBase):
+
+    name = base.Field('Name')
+    """The CPUHealth metric definition name"""
+
+    identity = base.Field('Id', required=True)
+    """The CPUHealth metric definition identity string"""
+
+    sensor_type = base.Field('SensorType')
+    """The type of sensor"""
+
+    metric_type = base.Field('MetricType')
+    """The type of metric"""
+
+    implementation = base.Field('Implementation')
+    """The implementation type of sensor"""
+
+    sensing_interval = base.Field('SensingInterval')
+    """The sensing interval"""
+
+    physical_context = base.Field('PhysicalContext')
+    """The physical context of this metric definition"""
+
+    units = base.Field('Units')
+    """The units of the sensor"""
+
+    min_reading_range = base.Field('MinReadingRange')
+    """The min reading range of this sensor"""
+
+    max_reading_range = base.Field('MaxReadingRange')
+    """The max reading range of this sensor"""
+
+    discrete_values = base.Field('DiscreteValues', adapter=list)
+    """The allowed discrete values"""
+
+    precision = base.Field('Precision')
+    """The precision of the sensor"""
+
+    calibration = base.Field('Calibration')
+    """The calibration of the sensor"""
+
+    isLinear = base.Field('IsLinear', adapter=bool)
+    """The boolean indicate this sensor is linear or not"""
+
+    calculable = base.Field('Calculable', adapter=bool)
+    """The variable indicate this sensor is calculable or not"""
+
+    data_type = base.Field('DataType')
+    """The type of the sensor data"""
+
+    accuracy = base.Field('Accuracy')
+    """The accuracy of the sensor"""
+
+    time_stamp_accuracy = base.Field('TimeStampAccuracy')
+    """The time stamp accuracy of the sensor"""
+
+    calculation_time_interval = base.Field('CalculationTimeInterval')
+    """The calculation time interval of the sensor"""
+
+    calculation_algorithm = base.Field('CalculationAlgorithm')
+    """The calculation algorithm of the sensor"""
+
+    calculation_parameters = base.Field('CalculationParameters', adapter=list)
+    """The calculation parameters of the sensor"""
+
+    wildcards = base.Field('Wildcards', adapter=list)
+    """The wildcards of the sensor"""
 
 
 class MetricDefinitionsCollection(base.ResourceCollectionBase):
 
-    def __init__(self, connector, path, redfish_version=None):
-        """A class representing a MetricDefinitionsCollection
-
-        :param connector: A Connector instance
-        :param path: The canonical path to the MetricDefinitions
-                     collection resource
-        :param redfish_version: The version of RedFish. Used to construct
-            the object according to schema of the given version.
-        """
-        super(MetricDefinitionsCollection, self).__init__(
-            connector, path, redfish_version)
-
-    def _get_metric_definition_type(self, identity):
-        """Get metric definition class
-
-        :param identity: The identity of the metric definition
-        :returns: The corresponding metric definition class
-        """
-        metric_def_name = os.path.basename(identity)
-
-        if metric_def_name == 'CPUTemperature':
-            return cpu_temperature.CPUTemperature
-        elif metric_def_name == 'CPUHealth':
-            return cpu_health.CPUHealth
-        elif metric_def_name == 'CPUBandwidth':
-            return cpu_bandwidth.CPUBandwidth
-        else:
-            raise NotImplementedError(
-                'Metric definition {0} is not supported yet.'
-                .format(metric_def_name))
-
-    def get_member(self, identity):
-        """Given the identity return a metric definition object
-
-        :param identity: The identity of the metric definition
-        :returns: The metric definition object
-        :raises: ResourceNotFoundError
-        """
-        # return cpu_temperature.CPUTemperature(
-        #     self._conn, identity, redfish_version=self.redfish_version)
-        metric_definition_type = self._get_metric_definition_type(identity)
-        return metric_definition_type(self._conn, identity,
-                                      redfish_version=self.redfish_version)
-
     @property
     def _resource_type(self):
-        pass
+        return MetricDefinition
